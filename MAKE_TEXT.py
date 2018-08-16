55f3c25d576f8cf05652514c8eb7778851414b1d
mklynn <lynn0032@gmail.com>
2018-08-15 21:36:55 -0500

    new structure

--- MAKE_TEXT.py
+++ MAKE_TEXT.py
@@ -1,6 +1,25 @@
 #change the title of the text here
-
 title = "Multivariable Calculus"
+
+weeks = []
+
+#edit materials for the first week here
+name = "Week 0: Review"
+topics = [ "ch00_review/1_vectors" , "ch00_review/2_dotProd", "ch00_review/3_crossProd", "ch00_review/4_matrices" , "ch00_review/5_planes"]
+online_hw = []
+written_hw = []
+professional_hw = []
+
+weeks.append({"name": name, "topics": topics, "online_hw": online_hw, "written_hw": written_hw, "professional_hw": professional_hw})
+
+#edit materials for the second week here
+name = "Week 1: Coordinate Systems"
+topics = [ "ch01_coordinate_systems/1_review" , "ch01_coordinate_systems/2_cylindrical_coords", "ch01_coordinate_systems/3_spherical_coords"]
+online_hw = []
+written_hw = []
+professional_hw = []
+
+weeks.append({"name": name, "topics": topics, "online_hw": online_hw, "written_hw": written_hw, "professional_hw": professional_hw})
 
 #you probably don't want to change the code below here
 
@@ -8,4 +27,32 @@ import generate_materials as gm
 
 import os
 
-gm.generate_text(title)
+topics = [ "chCURL_curl/N.1_curl_comp" , "chCURL_curl/N.2_curl_geo", "chCURL_curl/N.3_curl_connect"]
+
+online_hw = [ "chCURL_curl/N.3_curl_connect/problems/online/problem1.tex",
+             "chCURL_curl/N.2_curl_geo/problems/online/problem1.tex",
+             "chCURL_curl/N.2_curl_geo/problems/online/problem2.tex",
+             "chCURL_curl/N.2_curl_geo/problems/online/problem3.tex",
+             "chCURL_curl/N.3_curl_connect/problems/online/problem2.tex",
+             "chCURL_curl/N.3_curl_connect/problems/online/problem3.tex",
+             "chCURL_curl/N.3_curl_connect/problems/online/problem4.tex",
+             "chCURL_curl/N.3_curl_connect/problems/online/problem5.tex",
+             "chCURL_curl/N.3_curl_connect/problems/online/problem6.tex"]
+
+written_hw = [ "chCURL_curl/N.1_curl_comp/problems/written/problem1.tex",
+              "chCURL_curl/N.2_curl_geo/problems/written/problem1.tex",
+              "chCURL_curl/N.3_curl_connect/problems/written/problem1.tex"]
+
+professional_hw = [ "chCURL_curl/N.1_curl_comp/problems/professional/problem1.tex"]
+
+gm.generate_hw("week_CURL", online_hw, written_hw, professional_hw)
+
+gm.generate_all_problems("week_CURL", topics)
+
+gm.generate_topics("week_CURL", topics)
+
+gm.generate_group(weeks[0])
+gm.generate_group(weeks[1])
+
+gm.generate_text(title, weeks)
+
--- generate_materials.py
+++ generate_materials.py
@@ -1,7 +1,8 @@
 import os
 #import subprocess
 
-def generate_text(title):
+def generate_text(title, groups):
+    #generates the entire text.
 
     text = open("multivariable.tex", "w")
 
@@ -11,107 +12,102 @@ def generate_text(title):
     text.write("\\begin{abstract}\n")
     text.write("This is the testing ground for the UMTYMP Multivariable Calculus book.\n")
     text.write("\\end{abstract}\n")
-    text.write("\\maketitle\n")
-
-    for week in os.listdir("./schedule/"):
-        if os.path.isdir("./schedule/" + week):
-            text.write("\\input{./schedule/" + week + "/activities_list.tex}\n")
-            #subprocess.call("./schedule/" + week + "/TOPICS.py")    #I haven't tested this yet, don't have TOPICS.py in all weeks yet. Might want to use import or another method. Do I really want this here? I don't know.
+    text.write("\\maketitle\n\n")
+    
+    for group in groups:
+        text.write("\n\\section{" + group["name"] + "}\n\n")
+        activities = generate_group(group)
+        for activity in activities:
+            text.write("\\activity{" + activity + "}\n")
+    
 
     text.write("\\end{document}")
 
-    return;
+    return
 
-def generate_week(name, topics, online_hw, written_hw, professional_hw):
-    
-    #generates list of activities for this week.
-
-    activities_list = []
-    
-    activities_list.append("\\part{" + name + "}\n")
-
-    for section in topics:
-        path = "../../content/" + section + "/"
-        for file in os.listdir(path):
-            if file.endswith(".tex"):
-                activities_list.append("\\activity{" + section + "/" + file + "}\n")
-
-    long_path = os.path.abspath('.')
-    current_week = os.path.basename(long_path)
-
+def generate_hw(group, online_hw, written_hw, professional_hw):
     #generates homework problem sets
-
-    online_hw_list = []
-
-    for problem in online_hw:
-                    online_hw_list.append("\\input{../../content/" + problem + "}\n")
-
-    if len(online_hw_list) > 0:
-        online_hw = open("online_hw.tex", "w")
-
-        online_hw.write("\\documentclass{ximera}\n")
-        online_hw.write("\\title{Online Homework}\n")
-        online_hw.write("\\begin{document}\n")
-        online_hw.write("\\begin{abstract}\n")
-        online_hw.write("\\end{abstract}\n")
-        online_hw.write("\\maketitle\n")
-        online_hw.write("\\section{Online Problems}\n")
-
-        online_hw.writelines(online_hw_list)
-
-        online_hw.write("\\end{document}")
-
-        online_hw.close()
-            
-        activities_list.append("\\activity{./schedule/" + current_week + "/online_hw.tex}\n")
-
-    written_hw_list = []
-
-    for problem in written_hw:
-        written_hw_list.append("\\input{../../content/" + problem + "}\n")
     
-    if len(written_hw_list) > 0:
-        written_hw = open("written_hw.tex", "w")
+    activities = []
+    
+    if len(online_hw) > 0:
+        olhw_file = open("./auto_generated_text/" + group + "/online_hw.tex", "w")
+        activities.append("./auto_generated_text/" + group + "/online_hw.tex")
+    
+        olhw_file.write("\\documentclass{ximera}\n")
+        olhw_file.write("\\title{Online Homework}\n")
+        olhw_file.write("\\begin{document}\n")
+        olhw_file.write("\\begin{abstract}\n")
+        olhw_file.write("\\end{abstract}\n")
+        olhw_file.write("\\maketitle\n\n")
+    
+        for problem in online_hw:
+            path = "./content/" + problem
+            with open(path) as f:
+                for line in f:
+                    olhw_file.write(line)
+            olhw_file.write("\n\n")
 
-        written_hw.write("\\documentclass{ximera}\n")
-        written_hw.write("\\title{Written Homework}\n")
-        written_hw.write("\\begin{document}\n")
-        written_hw.write("\\begin{abstract}\n")
-        written_hw.write("\\end{abstract}\n")
-        written_hw.write("\\maketitle\n")
-        written_hw.write("\\section{Written Problems}\n")
+        olhw_file.write("\\end{document}")
+        olhw_file.close()
 
-        written_hw.writelines(written_hw_list)
+    if len(written_hw) > 0 or len(professional_hw) > 0:
+        whw_file = open("./auto_generated_text/" + group + "/written_hw.tex", "w")
+        activities.append("./auto_generated_text/" + group + "/written_hw.tex")
+    
+        whw_file.write("\\documentclass{ximera}\n")
+        whw_file.write("\\title{Written Homework}\n")
+        whw_file.write("\\begin{document}\n")
+        whw_file.write("\\begin{abstract}\n")
+        whw_file.write("\\end{abstract}\n")
+        whw_file.write("\\maketitle\n\n")
+    
+        if len(written_hw) > 0:
+            whw_file.write("\\section{Written Homework}\n\n")
+            for problem in written_hw:
+                path = "./content/" + problem
+                with open(path) as f:
+                    for line in f:
+                        whw_file.write(line)
+                whw_file.write("\n\n")
 
         if len(professional_hw) > 0:
-            for pro_problem in professional_hw:
-                written_hw.writelines(["\\section{Professional Problem}\n", "\\input{../../content/" + pro_problem + "}\n"])
+            whw_file.write("\\section{Professional Problem}\n\n")
+            for problem in professional_hw:
+                path = "./content/" + problem
+                with open(path) as f:
+                    for line in f:
+                        whw_file.write(line)
+                whw_file.write("\n\n")
+        
+        whw_file.write("\\end{document}")
+        whw_file.close()
+    
+    return activities
 
-        written_hw.write("\\end{document}")
-
-        written_hw.close()
-            
-        activities_list.append("\\activity{./schedule/" + current_week + "/written_hw.tex}\n")
-
-    #generates collection of all problems
+def generate_all_problems(group, topics):
+    #generates activity with all problems from topics
+    
+    activities = []
 
     online_problem_list = []
-
-    for section in topics:
-        path = "../../content/" + section + "/problems/online/"
-        for filename in os.listdir(path):
-            online_problem_list.append("\\input{../../content/" + section + "/problems/online/" + filename + "}\n")
-
-    written_problem_list = []
     
     for section in topics:
-        path = "../../content/" + section + "/problems/written/"
+        path = "./content/" + section + "/problems/online/"
         for filename in os.listdir(path):
-            written_problem_list.append("\\input{../../content/" + section + "/problems/written/" + filename + "}\n")
+            online_problem_list.append("./content/" + section + "/problems/online/" + filename)
+
+    written_problem_list = []
+
+    for section in topics:
+        path = "./content/" + section + "/problems/written/"
+        for filename in os.listdir(path):
+            written_problem_list.append("./content/" + section + "/problems/written/" + filename)
 
     if len(online_problem_list) > 0 or len(written_problem_list) > 0:
-        all_problems = open("all_problems.tex","w")
-
+        all_problems = open("./auto_generated_text/" + group + "/all_problems.tex","w")
+        activities.append("./auto_generated_text/" + group + "/all_problems.tex")
+    
         all_problems.write("\\documentclass{ximera}\n")
         all_problems.write("\\title{Extra Problems}\n")
         all_problems.write("\\begin{document}\n")
@@ -121,23 +117,62 @@ def generate_week(name, topics, online_hw, written_hw, professional_hw):
         
         if len(online_problem_list) > 0:
             all_problems.write("\\section{Online Problems}\n")
-
-            all_problems.writelines(online_problem_list)
-
+            
+            for problem in online_problem_list:
+                with open(problem) as f:
+                    for line in f:
+                        all_problems.write(line)
+                all_problems.write("\n\n")
+        
         if len(written_problem_list) > 0:
             all_problems.write("\\section{Written Problems}\n")
-
-            all_problems.writelines(written_problem_list)
-
+            
+            for problem in written_problem_list:
+                with open(problem) as f:
+                    for line in f:
+                        all_problems.write(line)
+                all_problems.write("\n\n")
+    
         all_problems.write("\\end{document}")
-
+        
         all_problems.close()
-            
-        activities_list.append("\\activity{./schedule/" + current_week + "/all_problems.tex}\n")
-            
-    activities = open("activities_list.tex", "w")
-    activities.writelines(activities_list)
-    activities.close()
 
-    return;
+    return activities
+
+def generate_topics(group, topics):
+    #copies topics files into auto_generated_text
+    
+    activities = []
+    
+    for section in topics:
+        path = "./content/" + section + "/"
+        for file in os.listdir(path):
+            if file.endswith(".tex"):
+                copy = open("./auto_generated_text/" + group + "/" + file,"w")
+                activities.append("./auto_generated_text/" + group + "/" + file)
+                with open(path + file) as f:
+                    for line in f:
+                        copy.write(line)
+                copy.close()
+    return activities
+
+def generate_group(group):
+    #builds all of the relevant files in auto_generated_text
+                                    
+    topics = group["topics"]
+    online_hw = group["online_hw"]
+    written_hw = group["written_hw"]
+    professional_hw = group["professional_hw"]
+    name = group["name"]
+    name = "".join(c for c in group["name"] if c.isalpha() or c.isdigit())
+    group["directory"] = name
+    
+    directory = "./auto_generated_text/" + name
+    
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    activities = generate_topics(name, topics) + generate_hw(name, online_hw, written_hw, professional_hw) + generate_all_problems(name, topics)
+
+    return activities
 
